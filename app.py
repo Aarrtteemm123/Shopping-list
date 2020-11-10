31b4f1fd0e9bd6955dfad4161a7f83152c4a595b
Artem <artem@gmail.com>
2020-11-10 22:37:48 +0200

    initial dialog

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivymd.app import MDApp
 from kivy.lang import Builder
+from kivymd.uix.button import MDFlatButton
+from kivymd.uix.dialog import MDDialog
 from kivymd.uix.menu import MDDropdownMenu
 from kivymd.uix.tab import MDTabsBase
 
-KV =  '''
+KV = '''
 BoxLayout:
     orientation:'vertical'
     
@@ -19,6 +22,18 @@ BoxLayout:
         MainScreen:
         SettingsScreen:
         AboutScreen:
+
+<Content>
+    orientation: "vertical"
+    spacing: "12dp"
+    size_hint_y: None
+    height: "120dp"
+
+    MDTextField:
+        hint_text: "City"
+
+    MDTextField:
+        hint_text: "Street"
   
 <MainScreen>:
     name: 'main' 
@@ -34,6 +49,12 @@ BoxLayout:
             MDLabel:
                 text: 'Active items'
                 halign: 'center'
+    MDFloatingActionButton:
+        icon: "plus"
+        pos_hint: {"center_x": .9, "center_y": .1}
+        md_bg_color: [0.11372549019607843, 0.9137254901960784, 0.7137254901960784, 1.0]
+        on_release: app.show_alert_dialog()
+
 
 <SettingsScreen>:
     name: 'settings'
@@ -58,16 +79,23 @@ BoxLayout:
         halign: 'center'
 '''
 
+
 class MainScreen(Screen):
     pass
+
 
 class SettingsScreen(Screen):
     pass
 
+
 class AboutScreen(Screen):
     pass
 
+
 class Tab(FloatLayout, MDTabsBase):
+    pass
+
+class Content(BoxLayout):
     pass
 
 class MyApp(MDApp):
@@ -77,16 +105,32 @@ class MyApp(MDApp):
         self.screen = Builder.load_string(KV)
         self.menu = MDDropdownMenu(
             caller=self.screen.ids.button,
-            #caller=self.screen.ids.screen_manager.get_screen('main').ids.button,
-            items=[{"text": 'Settings'},{"text": 'About project'}],
+            # caller=self.screen.ids.screen_manager.get_screen('main').ids.button,
+            items=[{"text": 'Settings'}, {"text": 'About project'}],
             width_mult=4
         )
         self.menu.bind(on_release=self.callback)
+        self.dialog = MDDialog(
+            title="Address:",
+            type="custom",
+            content_cls=Content(),
+            buttons=[
+                MDFlatButton(
+                    text="CANCEL", text_color=self.theme_cls.primary_color
+                ),
+                MDFlatButton(
+                    text="OK", text_color=self.theme_cls.primary_color
+                ),
+            ],
+        )
+
+    def show_alert_dialog(self):
+        self.dialog.open()
 
     def back_to_main_menu(self):
         self.root.ids.screen_manager.current = 'main'
 
-    def callback(self,instance_menu, instance_menu_item):
+    def callback(self, instance_menu, instance_menu_item):
         if instance_menu_item.text == 'Settings':
             self.root.ids.screen_manager.current = 'settings'
         if instance_menu_item.text == 'About project':
