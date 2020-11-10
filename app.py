fe83ff72f34f5d2ef32b79410976ff359cc3428c
Artem <artem@gmail.com>
2020-11-10 18:59:48 +0200

    Added menu

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 from kivymd.app import MDApp
 from kivy.lang import Builder
+from kivymd.uix.menu import MDDropdownMenu
 
-
-class MyApp(MDApp):
-
-    def build(self):
-        return Builder.load_string(
-            '''
+KV =  '''
 BoxLayout:
     orientation:'vertical'
 
     MDToolbar:
+        id: button
         title: 'Shopping list'
+        right_action_items: [["dots-vertical", lambda x: app.menu.open()]]
+    
+    MDScreen:
+        name: "screen_1"
 
     MDBottomNavigation:
 
@@ -33,7 +34,27 @@ BoxLayout:
                 text: 'Active items'
                 halign: 'center'
 '''
+
+class MyApp(MDApp):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.screen = Builder.load_string(KV)
+        menu_items = [
+            {"text": 'Settings'},{"text": 'About project'}
+        ]
+        self.menu = MDDropdownMenu(
+            caller=self.screen.ids.button,
+            items=menu_items,
+            width_mult=4
         )
+        self.menu.bind(on_release=self.callback)
+
+    def callback(self,instance_menu, instance_menu_item):
+        print(instance_menu, instance_menu_item.text)
+
+    def build(self):
+        return self.screen
 
 
 MyApp().run()
