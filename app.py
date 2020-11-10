3cf4b305542c43e6fd0256499de1a19cc4e155f9
Artem <artem@gmail.com>
2020-11-10 22:04:42 +0200

    Added new functions for change screen

--- app.py
+++ app.py
@@ -8,19 +8,20 @@ from kivymd.uix.tab import MDTabsBase
 KV =  '''
 BoxLayout:
     orientation:'vertical'
-    MDScreen:
-        ScreenManager: 
-            id:screen_manager
-            MainScreen:
-            SettingsScreen:
-            AboutScreen:
-             
-
+    
     MDToolbar:
         id: button
         title: 'Shopping list'
         right_action_items: [["dots-vertical", lambda x: app.menu.open()]]
-    
+            
+    ScreenManager: 
+        id:screen_manager
+        MainScreen:
+        SettingsScreen:
+        AboutScreen:
+  
+<MainScreen>:
+    name: 'main' 
     MDTabs:
         Tab:
             text: "History"
@@ -33,20 +34,25 @@ BoxLayout:
             MDLabel:
                 text: 'Active items'
                 halign: 'center'
-<MainScreen>:
-    name: 'main'
-    MDLabel:
-        text: 'Main'
-        halign: 'center'
 
 <SettingsScreen>:
     name: 'settings'
+    MDIconButton:
+        id: back_from_settings
+        icon: "arrow-left"
+        pos_hint: {"center_x": .075, "center_y": .95}
+        on_release: app.back_to_main_menu()
     MDLabel:
         text: 'Settings'
         halign: 'center'
         
 <AboutScreen>:
     name: 'about'
+    MDIconButton:
+        id: back_from_about
+        icon: "arrow-left"
+        pos_hint: {"center_x": .075, "center_y": .95}
+        on_release: app.back_to_main_menu()
     MDLabel:
         text: 'About'
         halign: 'center'
@@ -61,30 +67,31 @@ class SettingsScreen(Screen):
 class AboutScreen(Screen):
     pass
 
-
-
 class Tab(FloatLayout, MDTabsBase):
-    '''Class implementing content for a tab.'''
+    pass
 
 class MyApp(MDApp):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.screen = Builder.load_string(KV)
-        menu_items = [{"text": 'Settings'},{"text": 'About project'}]
         self.menu = MDDropdownMenu(
             caller=self.screen.ids.button,
-            items=menu_items,
+            #caller=self.screen.ids.screen_manager.get_screen('main').ids.button,
+            items=[{"text": 'Settings'},{"text": 'About project'}],
             width_mult=4
         )
         self.menu.bind(on_release=self.callback)
 
+    def back_to_main_menu(self):
+        self.root.ids.screen_manager.current = 'main'
+
     def callback(self,instance_menu, instance_menu_item):
-        print(instance_menu, instance_menu_item.text)
         if instance_menu_item.text == 'Settings':
             self.root.ids.screen_manager.current = 'settings'
         if instance_menu_item.text == 'About project':
             self.root.ids.screen_manager.current = 'about'
+        instance_menu.dismiss()
 
     def build(self):
         return self.screen
